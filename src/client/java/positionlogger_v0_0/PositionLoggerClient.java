43f833f3e82e2e554ae42e80f5742dab1adda9ba
tristanth03 <tristanthordarson@gmail.com>
2026-02-10 16:16:18 +0000

    Position Logger #0

---
+++ src/client/java/positionlogger_v0_0/PositionLoggerClient.java
@@ -0,0 +1,10 @@
+package positionlogger_v0_0;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PositionLoggerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
